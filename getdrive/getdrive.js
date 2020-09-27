53c8493dee65473226ed7c23b3e12519d6a23e20
Lewe Nguyen <lewe2k6@gmail.com>
2020-09-27 10:13:55 +0700

    Update getdrive.js

--- getdrive/getdrive.js
+++ getdrive/getdrive.js
@@ -24,21 +24,7 @@
 
     clipboard = new Clipboard('#copylinkbtn');
     clipboard.on('success', function(e) {
-      $.notify({
-        icon: 'glyphicon glyphicon-ok-circle',
-        title: 'Đã copy',
-        message: e.text,
-        url: e.text,
-        target: '_blank'
-      }, {
-        // settings
-        type: "success",
-        placement: {
-          from: "top",
-          align: "center"
-        }
-      });
-      // $.notify(e.text + " copied to clipboard.");
+      
 
       e.clearSelection();
     });
